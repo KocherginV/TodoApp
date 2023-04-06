a3c8fe0409f654cb045f19d71d447ccf3ebc7c60
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-04-06 12:03:21 +0200

    finish new todo ui

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import { TextInput, StyleSheet, Pressable } from 'react-native';
 import { Card, Text } from 'react-native-paper';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useState } from 'react';
 
 export const Todo: React.FC = () => {
+  const [date, setDate] = useState(new Date());
   return (
     <Card style={styles.todoContainer} mode={'elevated'}>
       <TextInput
@@ -19,14 +22,18 @@ export const Todo: React.FC = () => {
         maxLength={100}
         placeholder="New todo text"
       />
-      <Text>Datepicker</Text>
+      <Text style={styles.datePickerHeader}>When do you want to finish?</Text>
+      <DateTimePicker value={date} style={styles.datePicker} />
+      <Pressable style={styles.submitBtn}>
+        <Text style={styles.submitBtnText}>Submit</Text>
+      </Pressable>
     </Card>
   );
 };
 
 const styles = StyleSheet.create({
   todoContainer: {
-    height: 250,
+    minHeight: 270,
     marginLeft: 20,
     marginRight: 20,
     borderColor: 'black',
@@ -40,5 +47,32 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     margin: 10,
     padding: 5,
+  },
+  datePickerHeader: {
+    margin: 10,
+    marginBottom: 3,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+  datePicker: {
+    margin: 10,
+    marginTop: 3,
+    alignSelf: 'center',
+  },
+  submitBtn: {
+    borderColor: 'black',
+    borderRadius: 10,
+    borderWidth: 0.3,
+    padding: 10,
+    width: 180,
+    alignSelf: 'center',
+    marginTop: 30,
+    marginBottom: 10,
+    backgroundColor: '#e9e5e5',
+  },
+  submitBtnText: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    color: 'black',
   },
 });
