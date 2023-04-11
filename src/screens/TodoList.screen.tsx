0c5e24bb4eae230e6269cd0be043e59efbecad20
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-04-11 12:47:17 +0200

    introduced react context

--- src/screens/TodoList.screen.tsx
+++ src/screens/TodoList.screen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { useAppContext } from '../App.provider';
 
 export const List: React.FC = () => {
+  const appContext = useAppContext();
   return (
     <View style={styles.container}>
       <Text>List</Text>
+      <Text>{appContext.greeting}</Text>
     </View>
   );
 };
