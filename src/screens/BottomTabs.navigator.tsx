b9f723d7a6f37cad967a17bd9f9d5e9886e4aa14
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-04-18 17:51:33 +0200

    add svg icons

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -2,14 +2,41 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from './Home.screen';
 import { List } from './TodoList.screen';
+import { HomeIcon, ListIcon } from '../components/Icons';
+import { theme } from '../theme';
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="List" component={List} />
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: theme.colorDarkGrey,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        // eslint-disable-next-line react/no-unstable-nested-components
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') {
+            return <HomeIcon color={color} size={size} />;
+          }
+
+          if (route.name === 'List') {
+            return <ListIcon color={color} size={size} />;
+          }
+
+          return null;
+        },
+      })}>
+      <BottomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{ title: 'Add new Todo' }}
+      />
+      <BottomTabs.Screen
+        name="List"
+        component={List}
+        options={{ title: 'Created Todos' }}
+      />
     </BottomTabs.Navigator>
   );
 };
---
+++ src/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colorGrey: '#bbc1ca',
+  colorDarkBlue: '',
+  colorDarkGrey: '#222e40',
+};
